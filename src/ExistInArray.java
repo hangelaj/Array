f16b1d3b6e699c38ad15111e5401f93d5c627867
Student <jemal.hangela@outlook.com>
2023-03-29 05:56:10 +0100

    Renaming TestArray1 to TestArray_sum because I had simpler class named before

--- src/ExistInArray.java
+++ src/ExistInArray.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class ExistInArray {
     public static void main(String[] args) {
+        //creates and initialises an integer array numbers with a size of 5.
         int[] numbers = {2, 4, 6, 8, 10};
+        // initializes a boolean variable inArray to false,
+        // which will be used to keep track of whether the value entered by the user is inArray not found.
         boolean inArray = false;
 
+//using the Scanner class to ask the user to enter an integer value to search
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter an integer value to search for: ");
         int value = Integer.parseInt(reader.nextLine());
-
+//for loop to iterate over the elements in the numbers array,
+// checking each element to see if it matches the value entered by the user.
+// If a match is found, the program sets inArray as true.
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == value) {
                 inArray = true;
 
             }
         }
-
+//After all elements have been checked,
+// print outputs whether the value entered by the user was found in the array.
         if (inArray) {
             System.out.println("Value " + value + " found in array");
-        } else {
-            System.out.println("Value was not found in array");
+        }
+        // else print outputs Sorry value was not found in array
+        else {
+            System.out.println("Sorry value was not found in array");
         }
     }
 }
--- src/TestArray_sum.java
+++ src/TestArray_sum.java
@@ -3,7 +3,7 @@ public class TestArray_sum {
         int sum = 0; //initializes an integer variable sum to zero.
 
         //creates and initialises an integer array numbers with a size of 10.
-        int[] numbers = {2, 4, 6, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers = {2, 4, 6, 8, 5, 6, 7, 8, 9, 10};
 
         //for loop to iterate over the elements in the numbers array, and incrementing i++
         for (int i = 0; i < numbers.length; i++) {
